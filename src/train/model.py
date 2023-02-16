3519266e5cd6e2f71ebdf2dda8caa0eb4f7a0322
Arman Aminian <arman.aminian78@gmail.com>
2023-02-16 17:59:55 +0330

    feat: pretrained added to clip embeddings.

--- src/train/model.py
+++ src/train/model.py
@@ -1,11 +1,11 @@
-from transformers import CLIPModel, CLIPConfig
-from torch import nn
+from transformers import CLIPModel, CLIPConfig, CLIPVisionModel
+from transformers import AutoModel
 
 
 def get_clip_model(image_embedding_model, text_embedding_model, config):
     clip_config = CLIPConfig(text_config_dict=config,
                              vision_config_dict=config)
     clip = CLIPModel(config=clip_config)
-    clip.text_projection = nn.Identity()
-    clip.visual_projection = nn.Identity()
+    clip.text_projection = AutoModel.from_pretrained(text_embedding_model)
+    clip.visual_projection = CLIPVisionModel.from_pretrained(image_embedding_model)
     return clip
